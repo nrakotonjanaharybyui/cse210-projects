0b1e63fa1d6a8cc2c0c8cc973016d9fa3e215be6
nrakotonjanaharybyui <rak19008@byui.edu>
2024-09-21 14:21:55 -0600

    Update Files after Prep1 and Prep2 activities + Added a sandbox file for training and experiments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int gradePercent = int.Parse(Console.ReadLine());
+        string gradeLetter;
+        if(gradePercent >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if(gradePercent >= 80 && gradePercent < 90)
+        {
+            gradeLetter = "B";
+        }
+        else if(gradePercent >= 70 && gradePercent < 80)
+        {
+            gradeLetter = "C";
+        }
+        else if(gradePercent >= 60 && gradePercent < 70)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+        Console.WriteLine($"Your letter grade is {gradeLetter}");
+        if(gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass the course. You can do better next time!");
+        }
     }
 }
