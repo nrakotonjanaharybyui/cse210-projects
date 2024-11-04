ee43539c0904b91862320de35a0e7a0190e293f3
nrakotonjanaharybyui <rak19008@byui.edu>
2024-11-03 23:13:40 -0700

    Inheritance Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment ass1 = new Assignment("Samuel Bennett", "Multiplication");
+        MathAssignment ass2 = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "8-19");
+        WritingAssignment ass3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(ass3.GetSummary());
+        Console.WriteLine(ass3.GetWritingInformation());
     }
 }
