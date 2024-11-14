209dcb0cc46f0990af67db2a1d1b08b1078309b6
nrakotonjanaharybyui <rak19008@byui.edu>
2024-11-13 21:57:17 -0700

    W09 Prepare Learning

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,14 @@
+using System.Reflection.Metadata.Ecma335;
+
+public class Circle : Shape
+{
+    double _radius;
+    public Circle(string color, double radius):base(color)
+    {
+        _radius = radius;
+    }
+    public override double GetArea()
+    {
+        return  Math.Pow(_radius, 2) * Math.PI;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = [];
+        Square sq1 = new Square("Blue", 12.5);
+        Rectangle rec1 = new Rectangle("Green", 5.2, 3.4);
+        Circle cir1 = new Circle("Red", 12);
+        shapes.Add(sq1);
+        shapes.Add(rec1);
+        shapes.Add(cir1);
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
